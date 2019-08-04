672a16429878217f3123277efabae20b8f8dc777
Gokhur ARIKBOGA <gokhur@gokhur.net>
2019-08-04 16:11:26 +0300

    Initial Commit

---
+++ UzayYakala/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UzayYakala
+//
+//  Created by Gökhür ARIKBOGA on 4.08.2019.
+//  Copyright © 2019 Gokhur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
